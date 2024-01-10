20aae7ab505b1b341b01f4526c43abdbb1e0e3fe
pizza-777 <doctor4891@gmail.com>
2024-01-10 17:02:26 +0200

    modexp

---
+++ rsa/rsa_starter_one.js
@@ -0,0 +1,12 @@
+//modulus exponentiation
+function expmod( base, exp, mod ){
+    if (exp == 0) return 1;
+    if (exp % 2 == 0){
+      return Math.pow( expmod( base, (exp / 2), mod), 2) % mod;
+    }
+    else {
+      return (base * expmod( base, (exp - 1), mod)) % mod;
+    }
+  }
+
+  console.log(expmod(101,17,22663))
